e21717ef2c60f53daad5183d7ad1f719d5267adb
iakrevetkho <shchukin.ivan.v@gmail.com>
2022-02-12 19:17:36 +0300

    Mock simulate method

--- simulator/usecase/usecase.go
+++ simulator/usecase/usecase.go
@@ -2,11 +2,10 @@ package usecase
 
 import (
 	"github.com/iakrevetkho/cost/domain"
-	"github.com/sirupsen/logrus"
 )
 
 type SimulatorUsecase interface {
-	Simulate(sc domain.Scheme) (*domain.Report, error)
+	Simulate(dfc domain.DataFlowChain) (*domain.Report, error)
 }
 
 type simulatorUsecase struct {
@@ -17,26 +16,26 @@ func NewSimulatorUsecase() SimulatorUsecase {
 	return tuc
 }
 
-func (suc *simulatorUsecase) Simulate(sc domain.Scheme) (*domain.Report, error) {
+func (suc *simulatorUsecase) Simulate(dfc domain.DataFlowChain) (*domain.Report, error) {
 	r := domain.NewReport()
 
-	// 1. Calc base consumption
-	for _, node := range sc.Roots {
-		switch node.Type {
-		case domain.NodeType_Component:
-			r.Roots = append(r.Roots, &domain.NodeReport{
-				CpuUsage: node.Model,
-			})
+	// // 1. Calc base consumption
+	// for _, node := range sc.Roots {
+	// 	switch node.Type {
+	// 	case domain.NodeType_Component:
+	// 		r.Roots = append(r.Roots, &domain.NodeReport{
+	// 			CpuUsage: node.Model,
+	// 		})
 
-		case domain.NodeType_Custom:
-			// TODO Implement
-			logrus.Debug("skip consumption calc for Custom node")
-		case domain.NodeType_Client:
-			logrus.Debug("skip consumption calc for Client node")
-		default:
-			return nil, domain.ErrUnknownNodeType
-		}
-	}
+	// 	case domain.NodeType_Custom:
+	// 		// TODO Implement
+	// 		logrus.Debug("skip consumption calc for Custom node")
+	// 	case domain.NodeType_Client:
+	// 		logrus.Debug("skip consumption calc for Client node")
+	// 	default:
+	// 		return nil, domain.ErrUnknownNodeType
+	// 	}
+	// }
 
 	return r, nil
 }
